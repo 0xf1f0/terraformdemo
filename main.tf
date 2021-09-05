dbc152d6dd870bff4ab160baf491e2748c942958
0xf1f0 <oobey1@aol.com>
2021-09-05 18:08:37 -0400

    Changed ami to Ubuntu LTS

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-087c17d1fe0178315"
+  ami           = "ami-0747bdcabd34c712a"
   instance_type = "t2.micro"
 
   tags = {
